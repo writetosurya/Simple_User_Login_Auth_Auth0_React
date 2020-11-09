1e7687d502ff98e3fd74b4f4141fb3a478b525b6
writetosurya <62762212+writetosurya@users.noreply.github.com>
2020-11-09 16:36:24 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import JSONPretty from 'react-json-pretty';
-
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
-
   return (
     isAuthenticated && ( 
      <div>
@@ -17,5 +15,4 @@ const Profile = () => {
     )
   )
 }
-
 export default Profile
